00884f22a5805d4ebd321446c2659417f164fed8
jx <jinxincandor@163.com>
2023-09-17 14:50:43 +0800

    first commit

---
+++ 01_helloworld/src/main/java/org/example/MainApplication.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author JX
+ * @ClassName: org.example.MainApplication
+ * @Create 2023-09-17 14:44
+ * @Description: TODO
+ */
+// 主程序类
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ 01_helloworld/src/main/java/org/example/controller/Hello.java
@@ -0,0 +1,22 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author JX
+ * @ClassName: org.example.controller.Hello
+ * @Create 2023-09-17 14:45
+ * @Description: TODO
+ */
+//@ResponseBody //返回字符串，不是一个网页
+//@Controller
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String handle01() {
+        return "Hello,springboot 2";
+    }
+}
